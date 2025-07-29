3a262520db9ee9d5d5142578c984a8534fc08c64
jinoc <choiae369@gmail.com>
2025-07-29 22:27:08 +0900

    week9-연산자 끼워기 문제 주석 삭제

---
+++ jincheol/week9/study-bruteForce2.js
@@ -0,0 +1,9 @@
+// 문제 링크: [스타트와 링크](https://www.acmicpc.net/problem/14889)
+
+// 실행: node jincheol/week9/study-bruteForce1.js
+
+const fs = require('fs');
+const input = fs
+  .readFileSync('./jincheol/week9/input2.txt')
+  .toString()
+  .split('\n');
