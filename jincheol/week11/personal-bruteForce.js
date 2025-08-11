f1b97e6b7db65c2fdf08fffb79771e411abf33ce
jinoc <choiae369@gmail.com>
2025-08-11 22:58:14 +0900

    week11-스도쿠

--- jincheol/week11/personal-bruteForce.js
+++ jincheol/week11/personal-bruteForce.js
@@ -9,3 +9,57 @@ const input = fs
   .toString()
   .trim()
   .split('\n');
+
+const sudoku = input.map((row) => row.split(' ').map(Number));
+
+const emptySpaces = [];
+
+for (let row = 0; row < 9; row++) {
+  for (let col = 0; col < 9; col++) {
+    const space = sudoku[row][col];
+    if (space === 0) emptySpaces.push([row, col]);
+  }
+}
+
+/**
+ *
+ * @param {number} emptyIdx 비어있는 칸 인덱스(emptySpaces의)
+ * @returns
+ */
+const dfs = (emptyIdx) => {
+  if (emptySpaces.length === emptyIdx) {
+    sudoku.forEach((row) => {
+      console.log(row.join(' '));
+    });
+    return true;
+  }
+
+  const [row, col] = emptySpaces[emptyIdx];
+  for (let number = 1; number < 10; number++) {
+    if (isOK(row, col, number)) {
+      sudoku[row][col] = number;
+      if (dfs(emptyIdx + 1)) return true;
+      sudoku[row][col] = 0;
+    }
+  }
+};
+
+const isOK = (row, col, number) => {
+  for (let i = 0; i < 9; i++) {
+    const isSameNumber = sudoku[row][i] === number || sudoku[i][col] === number;
+    if (isSameNumber) return false;
+  }
+
+  const squareStartRow = Math.floor(row / 3) * 3;
+  const squareStartCol = Math.floor(col / 3) * 3;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      const squareNum = sudoku[squareStartRow + i][squareStartCol + j];
+      if (squareNum === number) return false;
+    }
+  }
+
+  return true;
+};
+
+dfs(0);
