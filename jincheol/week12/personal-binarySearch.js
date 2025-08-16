7c5c636c2808222eda3a6d06e0af556537eddd11
jinoc <choiae369@gmail.com>
2025-08-17 00:38:25 +0900

    week12-랜선 자르기

---
+++ jincheol/week12/personal-binarySearch.js
@@ -0,0 +1,11 @@
+// 문제 링크: [수 이어 쓰기2](https://www.acmicpc.net/problem/1790)
+
+// 실행: node jincheol/week12/personal-binarySearch.js
+
+const fs = require('fs');
+const input = fs
+  // .readFileSync('/dev/stdin')
+  .readFileSync('./jincheol/week12/input3.txt')
+  .toString()
+  .trim()
+  .split('\n');
---
+++ jincheol/week12/study-binarySearch2.js
@@ -0,0 +1,11 @@
+// 문제 링크: [나무 자르기](https://www.acmicpc.net/problem/2805)
+
+// 실행: node jincheol/week12/study-binarySearch2.js
+
+const fs = require('fs');
+const input = fs
+  // .readFileSync('/dev/stdin')
+  .readFileSync('./jincheol/week12/input2.txt')
+  .toString()
+  .trim()
+  .split('\n');
