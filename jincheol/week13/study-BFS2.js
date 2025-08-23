8cf157ccabb974060ed1c4108ee6eb8a1a7b5a99
jinoc <choiae369@gmail.com>
2025-08-23 23:31:32 +0900

    week13-4연산 주석 추가

--- jincheol/week13/study-BFS2.js
+++ jincheol/week13/study-BFS2.js
@@ -16,7 +16,7 @@ const input = fs
  * @param {number} t 목표하는 수
  */
 const solution = (s, t) => {
-  if (s === t) return 0;
+  if (s === t) return 0; // s와 t가 같은 경우 0 return
 
   /**
    *
@@ -37,31 +37,37 @@ const solution = (s, t) => {
     }
   };
 
-  const queue = [[s, '']];
-  const operators = ['*', '+', '-', '/'];
-  const MAX = 1_000_000_000;
-  const calcHistory = new Set();
-  calcHistory.add(s);
+  const queue = [[s, '']]; // BFS를 위한 큐 [초기 숫자, 연산자]
+  const operators = ['*', '+', '-', '/']; // 연산자들
+  const MAX = 1_000_000_000; // 최댓값 (10^9)
+  const calcHistory = new Set(); // 계산 기록
+  calcHistory.add(s); // 첫 s 추가
 
+  // BFS 시작
   while (queue.length) {
-    const [curNum, operatorHistory] = queue.shift();
+    const [curNum, operatorHistory] = queue.shift(); // 계산할 숫자와 연산자 기록
 
+    // 연산자 순회
     for (let operator of operators) {
+      // 현재 숫자가 0이 아닐 때만 / 를 사용할 수 있음
       if (operator === '/' && curNum === 0) continue;
 
-      const result = calc(curNum, operator);
+      const result = calc(curNum, operator); // 계산
+      // 계산 결과가 목표하는 숫자일 경우
       if (result === t) {
-        const resultOperator = operatorHistory + operator;
+        const resultOperator = operatorHistory + operator; // 연산자를 추가하고 return
         return resultOperator;
       }
 
+      // 계산 결과가 최댓값 초과이거나 계산 결과가 이미 계산한 경우일 때 continue (값은 값 반복이라)
       if (result > MAX || calcHistory.has(result)) continue;
 
-      queue.push([result, operatorHistory + operator]);
-      calcHistory.add(result);
+      queue.push([result, operatorHistory + operator]); // 큐에 다음 숫자와 연산자 기록 추가
+      calcHistory.add(result); // 계산 기록에 추가
     }
   }
 
+  // 큐가 전부 비워졌음에도 여기까지 오면 바꿀 수 없는 경우라 -1 return
   return -1;
 };
 
