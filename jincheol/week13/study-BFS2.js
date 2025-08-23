e08dae44098e4d376d9dac27959b919a06f3316a
jinoc <choiae369@gmail.com>
2025-08-23 17:14:18 +0900

    week13-뱀과 사다리 게임

---
+++ jincheol/week13/study-BFS2.js
@@ -0,0 +1,11 @@
+// 문제 링크: [4연산](https://www.acmicpc.net/problem/14395)
+
+// 실행: node jincheol/week13/study-BFS2.js
+
+const fs = require('fs');
+const input = fs
+  // .readFileSync('/dev/stdin')
+  .readFileSync('./jincheol/week13/input2.txt')
+  .toString()
+  .trim()
+  .split('\n');
