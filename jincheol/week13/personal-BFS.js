2f5da77f934d8700aaeef23897535443376f15bf
jinoc <choiae369@gmail.com>
2025-08-23 23:17:41 +0900

    week13-4연산

---
+++ jincheol/week13/personal-BFS.js
@@ -0,0 +1,11 @@
+// 문제 링크: [](https://www.acmicpc.net/problem/)
+
+// 실행: node jincheol/week13/personal-BFS.js
+
+const fs = require('fs');
+const input = fs
+  // .readFileSync('/dev/stdin')
+  .readFileSync('./jincheol/week13/input3.txt')
+  .toString()
+  .trim()
+  .split('\n');
--- jincheol/week13/study-BFS2.js
+++ jincheol/week13/study-BFS2.js
@@ -9,3 +9,61 @@ const input = fs
   .toString()
   .trim()
   .split('\n');
+
+/**
+ *
+ * @param {number} s 정수
+ * @param {number} t 목표하는 수
+ */
+const solution = (s, t) => {
+  if (s === t) return 0;
+
+  /**
+   *
+   * @param {number} num 기존 계산값
+   * @param {string} operator 연산자
+   * @returns {number} 계산한 결과값
+   */
+  const calc = (num, operator) => {
+    switch (operator) {
+      case '+': // 덧셈
+        return num + num;
+      case '-': // 뺄셈
+        return num - num;
+      case '*': // 곱셈
+        return num * num;
+      case '/': // 나눗셈
+        return num / num;
+    }
+  };
+
+  const queue = [[s, '']];
+  const operators = ['*', '+', '-', '/'];
+  const MAX = 1_000_000_000;
+  const calcHistory = new Set();
+  calcHistory.add(s);
+
+  while (queue.length) {
+    const [curNum, operatorHistory] = queue.shift();
+
+    for (let operator of operators) {
+      if (operator === '/' && curNum === 0) continue;
+
+      const result = calc(curNum, operator);
+      if (result === t) {
+        const resultOperator = operatorHistory + operator;
+        return resultOperator;
+      }
+
+      if (result > MAX || calcHistory.has(result)) continue;
+
+      queue.push([result, operatorHistory + operator]);
+      calcHistory.add(result);
+    }
+  }
+
+  return -1;
+};
+
+const [s, t] = input[0].split(' ').map(Number);
+console.log(solution(s, t));
