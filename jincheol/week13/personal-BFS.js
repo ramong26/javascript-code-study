55913e7c17f916cd26303998182c95bf2c4fcce4
jinoc <choiae369@gmail.com>
2025-08-24 16:48:02 +0900

    week13-데스 나이트

--- jincheol/week13/personal-BFS.js
+++ jincheol/week13/personal-BFS.js
@@ -1,4 +1,4 @@
-// 문제 링크: [](https://www.acmicpc.net/problem/)
+// 문제 링크: [데스 나이트](https://www.acmicpc.net/problem/16948)
 
 // 실행: node jincheol/week13/personal-BFS.js
 
@@ -9,3 +9,44 @@ const input = fs
   .toString()
   .trim()
   .split('\n');
+
+/**
+ *
+ * @param {number} N 보드의 크기
+ * @param {[number, number]} start 시작하는 칸 [r1, c1]
+ * @param {[number, number]} end 목표하는 칸 [r2, c2]
+ * @returns
+ */
+const solution = (N, start, end) => {
+  const board = Array.from({ length: N }, () =>
+    Array.from({ length: N }, () => -1)
+  );
+  const goR = [-2, -2, 0, 0, 2, 2];
+  const goC = [-1, 1, -2, 2, -1, 1];
+
+  const queue = [start];
+  board[start[0]][start[1]] = 0;
+
+  while (queue.length) {
+    const [r, c] = queue.shift();
+    if (r === end[0] && c === end[1]) return board[r][c];
+
+    for (let i = 0; i < 6; i++) {
+      const nextR = r + goR[i];
+      const nextC = c + goC[i];
+
+      if (nextR >= N || nextR < 0 || nextC >= N || nextC < 0) continue;
+
+      if (board[nextR][nextC] === -1) {
+        board[nextR][nextC] = board[r][c] + 1;
+        queue.push([nextR, nextC]);
+      }
+    }
+  }
+
+  return -1;
+};
+
+const N = parseInt(input.shift());
+const [r1, c1, r2, c2] = input[0].split(' ').map(Number);
+console.log(solution(N, [r1, c1], [r2, c2]));
