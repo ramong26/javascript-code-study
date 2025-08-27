084e47df086ef4793d20f89cb7fa2aeac083ebfd
jinoc <choiae369@gmail.com>
2025-08-28 00:31:10 +0900

    week14-ATM

---
+++ jincheol/week14/personal-greedy.js
@@ -0,0 +1,11 @@
+// 문제 링크: [동전 뒤집기](https://www.acmicpc.net/problem/1285)
+
+// 실행: node jincheol/week14/personal-greedy.js
+
+const fs = require('fs');
+const input = fs
+  // .readFileSync('/dev/stdin')
+  .readFileSync('./jincheol/week14/input3.txt')
+  .toString()
+  .trim()
+  .split('\n');
---
+++ jincheol/week14/study-greedy2.js
@@ -0,0 +1,11 @@
+// 문제 링크: [회의실 배정](https://www.acmicpc.net/problem/1931)
+
+// 실행: node jincheol/week14/study-greedy2.js
+
+const fs = require('fs');
+const input = fs
+  // .readFileSync('/dev/stdin')
+  .readFileSync('./jincheol/week14/input2.txt')
+  .toString()
+  .trim()
+  .split('\n');
