b63f805d84c97613598f0ca7a41ee628a38a7ac4
jinoc <choiae369@gmail.com>
2025-08-29 01:06:38 +0900

    week14-회의실 배정

--- jincheol/week14/study-greedy2.js
+++ jincheol/week14/study-greedy2.js
@@ -9,3 +9,32 @@ const input = fs
   .toString()
   .trim()
   .split('\n');
+
+/**
+ *
+ * @param {number} N
+ * @param {Array<[number, number]>} meetings
+ */
+const solution = (N, meetings) => {
+  // const ascendingMeetings = meetings.sort((a, b) => a[1] - b[1]);
+  const ascendingMeetings = meetings.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    return a[1] - b[1];
+  });
+
+  let meetingCount = 0;
+  let lastMeetingEnd = 0;
+
+  for (let [startTime, endTime] of ascendingMeetings) {
+    if (startTime >= lastMeetingEnd) {
+      meetingCount++;
+      lastMeetingEnd = endTime;
+    }
+  }
+
+  return meetingCount;
+};
+
+const N = parseInt(input.shift());
+const meetings = input.map((v) => v.split(' ').map(Number));
+console.log(solution(N, meetings));
