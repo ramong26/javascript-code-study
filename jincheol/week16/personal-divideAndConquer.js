8e13c108208cbd844a24981577eebc7a7343c607
jinoc <choiae369@gmail.com>
2025-09-13 19:57:06 +0900

    week16-배열 합치기

--- jincheol/week16/personal-divideAndConquer.js
+++ jincheol/week16/personal-divideAndConquer.js
@@ -9,3 +9,21 @@ const input = fs
   .toString()
   .trim()
   .split('\n');
+/**
+ *
+ * @param {number} N
+ * @param {number} M
+ * @param {number[]} A
+ * @param {number[]} B
+ * @returns
+ */
+const solution = (N, M, A, B) => {
+  return A.concat(B)
+    .sort((a, b) => a - b)
+    .join(' ');
+};
+
+const [N, M] = input.shift().split(' ').map(Number);
+const A = input.shift().split(' ').map(Number);
+const B = input.shift().split(' ').map(Number);
+console.log(solution(N, M, A, B));
