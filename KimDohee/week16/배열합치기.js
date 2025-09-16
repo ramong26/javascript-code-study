778b8b6e954e35248f06c6adcf23c5837898cecf
doy00 <dhkm201@gmail.com>
2025-09-16 21:46:03 +0900

    week16 - 분할정보 - 배열합치기

---
+++ KimDohee/week16/배열합치기.js
@@ -0,0 +1,46 @@
+/**
+ * 분할정복 - 배열 합치기(https://www.acmicpc.net/problem/11728)
+ * 정렬되어있는 두 배열 A와 B가 주어진다. 두 배열을 합친 다음 정렬해서 출력하는 프로그램을 작성하시오.
+ */
+
+function solution(input) {
+  const lines = input.trim().split('\n');
+  const [N, M] = lines[0].split(' ').map(Number);
+  const A = lines[1].split(' ').map(Number);
+  const B = lines[2].split(' ').map(Number);
+
+  const result = [];
+  let i = 0, j = 0;
+
+  // 두 배열을 비교하며 result에 병합
+  while (i < N && j < M) {
+    if (A[i] < B[j]) {
+      result.push(A[i])
+      i++;
+    } else {
+      result.push(B[j])
+      j++;
+    }
+  }
+
+  // 남은 원소들 처리
+  while (i < N) {
+    result.push(A[i]);
+    i++;
+  }
+
+  while (j < M) {
+    result.push(B[j]);
+    j++;
+  }
+
+  return result.join(' ');
+}
+
+// 백준 제출용 코드
+const fs = require('fs');
+const path = require('path');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : path.join(__dirname, 'input.txt');
+const input = fs.readFileSync(filePath).toString();
+
+console.log(solution(input));
