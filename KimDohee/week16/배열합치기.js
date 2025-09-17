63ec4746a97bd398e14482fdf720d4b4bd0a588c
doy00 <dhkm201@gmail.com>
2025-09-17 13:48:53 +0900

    배열합치기 주석 추가

--- KimDohee/week16/배열합치기.js
+++ KimDohee/week16/배열합치기.js
@@ -1,6 +1,13 @@
 /**
  * 분할정복 - 배열 합치기(https://www.acmicpc.net/problem/11728)
  * 정렬되어있는 두 배열 A와 B가 주어진다. 두 배열을 합친 다음 정렬해서 출력하는 프로그램을 작성하시오.
+ * 
+ * 투포인터 예시)
+ * A = [3, 5]
+ *      ↑
+ * B = [2, 9]
+ *      ×  ↑  (다음으로 이동)
+ * result = [2]
  */
 
 function solution(input) {
@@ -24,11 +31,13 @@ function solution(input) {
   }
 
   // 남은 원소들 처리
+  // B 배열을 모두 처리했고 A 배열에 원소가 남아있을 때
   while (i < N) {
     result.push(A[i]);
     i++;
   }
 
+  // A 배열을 모두 처리했고 B 배열에 원소가 남아있을 때
   while (j < M) {
     result.push(B[j]);
     j++;
