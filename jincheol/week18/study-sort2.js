3c3332e665bf641e8b5f82b22fd4ea039f290dad
jinoc <choiae369@gmail.com>
2025-09-25 23:19:17 +0900

    week18-카드

--- jincheol/week18/study-sort2.js
+++ jincheol/week18/study-sort2.js
@@ -9,3 +9,35 @@ const input = fs
   .toString()
   .trim()
   .split('\n');
+
+/**
+ *
+ * @param {number} N
+ * @param {number[]} cards
+ */
+const solution = (N, cards) => {
+  const cardMap = new Map();
+  for (const card of cards) {
+    cardMap.set(card, (cardMap.get(card) || 0) + 1);
+  }
+
+  let maxCount = 0;
+  let answer = Infinity;
+  for (const [number, count] of cardMap) {
+    if (maxCount < count) {
+      answer = number;
+      maxCount = count;
+      continue;
+    }
+
+    if (maxCount === count && number < answer) {
+      answer = number;
+    }
+  }
+
+  return answer.toString();
+};
+
+const N = parseInt(input.shift());
+const cards = input.map((v) => BigInt(v.trim()));
+console.log(solution(N, cards));
