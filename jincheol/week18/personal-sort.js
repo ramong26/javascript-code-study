7f2787ff415746a3d6bbde8f262afe9650e66364
jinoc <choiae369@gmail.com>
2025-09-26 17:08:15 +0900

    week18-나이순 정렬

--- jincheol/week18/personal-sort.js
+++ jincheol/week18/personal-sort.js
@@ -1,6 +1,6 @@
-// 문제 링크: [](https://www.acmicpc.net/problem/)
+// 문제 링크: [나이순 정렬](https://www.acmicpc.net/problem/10814)
 
-// 실행: node jincheol/week18/personal-sort1.js
+// 실행: node jincheol/week18/personal-sort.js
 
 const fs = require('fs');
 const input = fs
@@ -9,3 +9,31 @@ const input = fs
   .toString()
   .trim()
   .split('\n');
+
+/**
+ *
+ * @param {number} N
+ * @param {[number, string][]} members
+ */
+const solution = (N, members) => {
+  members.sort((a, b) => {
+    if (a[0] !== b[0]) return a[0] - b[0];
+    return 0;
+  });
+
+  let answer = '';
+
+  for (const member of members) {
+    answer += member.join(' ') + '\n';
+  }
+
+  return answer;
+};
+
+const N = parseInt(input.shift());
+const members = input.map((v) => {
+  const [age, name] = v.trim().split(' ');
+  return [+age, name];
+});
+
+console.log(solution(N, members));
