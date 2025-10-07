1ea56552a8a0b2008c5857c6f0f0d62f3630f398
jinoc <choiae369@gmail.com>
2025-10-07 15:06:11 +0900

    week19-A와 B 2

--- jincheol/week19/personal-greedy3.js
+++ jincheol/week19/personal-greedy3.js
@@ -1,4 +1,4 @@
-// 문제 링크: [AB](https://www.acmicpc.net/problem/12970)
+// 문제 링크: [A와 B 2](https://www.acmicpc.net/problem/12919)
 
 // 실행: node jincheol/week19/personal-greedy3.js
 
@@ -9,3 +9,41 @@ const input = fs
   .toString()
   .trim()
   .split('\n');
+
+/**
+ * T -> S 역순으로 변환하기, 조건 2개 모두 적용 가능한 경우가 있기에 DFS 사용
+ * @param {string} S 시작 문자열
+ * @param {string} T 목표하는 문자열
+ */
+const solution = (S, T) => {
+  let answer = 0; // 가능 여부
+
+  /**
+   *
+   * @param {string} curStr 현재 문자열
+   * @returns
+   */
+  const dfs = (curStr) => {
+    if (answer === 1) return;
+    if (curStr.length === S.length) {
+      if (curStr === S) answer = 1;
+      return;
+    }
+
+    if (curStr.endsWith('A')) {
+      dfs(curStr.slice(0, -1));
+    }
+
+    if (curStr.startsWith('B')) {
+      const nextStr = curStr.slice(1).split('').reverse().join('');
+      dfs(nextStr);
+    }
+  };
+
+  dfs(T);
+
+  return answer;
+};
+
+const [S, T] = input.map((v) => v.trim());
+console.log(solution(S, T));
