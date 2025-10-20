bf5b1821f18a593bed97391dde77676c61fe974a
jinoc <choiae369@gmail.com>
2025-10-20 21:32:00 +0900

    week21-A -> B

--- jincheol/week21/study-bruteForce2.js
+++ jincheol/week21/study-bruteForce2.js
@@ -10,4 +10,31 @@ const input = fs
   .trim()
   .split('\n');
 
-const solution = () => {};
+/**
+ * 역순 변환
+ * @param {string} A
+ * @param {string} B
+ */
+const solution = (A, B) => {
+  let count = 1;
+
+  while (+B > +A) {
+    if (B.endsWith('1')) {
+      B = B.slice(0, -1);
+      count++;
+    } else if (+B % 2 === 0) {
+      B = String(B / 2);
+      count++;
+    } else {
+      count = -1;
+      break;
+    }
+  }
+
+  if (B !== A) return -1;
+
+  return count;
+};
+
+const [A, B] = input[0].split(' ');
+console.log(solution(A, B));
