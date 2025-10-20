9228e3980924519d88f25c60fae19902292feb55
jinoc <choiae369@gmail.com>
2025-10-20 21:53:04 +0900

    week21-A -> B 주석 추가

--- jincheol/week21/study-bruteForce2.js
+++ jincheol/week21/study-bruteForce2.js
@@ -11,26 +11,35 @@ const input = fs
   .split('\n');
 
 /**
- * 역순 변환
+ * B를 A로 역순 변환
  * @param {string} A
  * @param {string} B
  */
 const solution = (A, B) => {
-  let count = 1;
+  let count = 1; // 변환 횟수
 
+  // dfs로 시도했으나 실패함 -> bfs로 시도
+  // 숫자 B가 A보다 크면 탐색
   while (+B > +A) {
+    // 숫자의 마지막이 1이면
     if (B.endsWith('1')) {
-      B = B.slice(0, -1);
-      count++;
-    } else if (+B % 2 === 0) {
-      B = String(B / 2);
-      count++;
-    } else {
+      B = B.slice(0, -1); // 마지막 숫자 1 제거
+      count++; // 변환 횟수 증가
+    }
+    // 마지막 숫자가 1이 아니고 짝수일 경우
+    else if (+B % 2 === 0) {
+      B = String(B / 2); // 2로 나누기
+      count++; // 변환 횟수 증가
+    }
+    // 둘 다 아닌 경우는 변환이 불가능한 경우
+    else {
       count = -1;
       break;
     }
   }
 
+  // 역으로 변환을 마친 숫자 B가 A보다 같거나 작은 경우
+  // 다르면 -1 return
   if (B !== A) return -1;
 
   return count;
