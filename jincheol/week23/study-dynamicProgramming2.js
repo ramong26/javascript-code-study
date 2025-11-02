68e04fc88a3d57bee68ba9e1a8f3368be328aa9d
jinoc <choiae369@gmail.com>
2025-11-03 02:42:53 +0900

    week23-점프 주석 추가

--- jincheol/week23/study-dynamicProgramming2.js
+++ jincheol/week23/study-dynamicProgramming2.js
@@ -9,26 +9,28 @@
  * @returns
  */
 const solution = (N, board) => {
-  const dp = Array.from({ length: N }, () => new Array(N).fill(0n));
+  const dp = Array.from({ length: N }, () => new Array(N).fill(0n)); // BigInt를 사용하지 않으면 마지막에 틀림
   dp[0][0] = 1n;
 
+  // 게임 판 순회
   for (let r = 0; r < N; r++) {
     for (let c = 0; c < N; c++) {
+      // 마지막 칸에 도착했을 때 계산을 하면 move는 0임. 따라서 아래 계산에서 정답 * 3이 된다.
       if (r === N - 1 && c === N - 1) continue;
 
-      const currentPath = dp[r][c];
-      if (currentPath === 0n) continue;
+      const currentPath = dp[r][c]; // 현재 칸에 도달 가능한 경우의 수
+      if (currentPath === 0n) continue; // 0이면 건너뛰기
 
-      const move = board[r][c];
-      const nr = r + move;
-      if (nr < N) dp[nr][c] += currentPath;
+      const move = board[r][c]; // 현재 칸에서 점프할 수 있는 거리
+      const nr = r + move; // 아래로 점프할 때 도착 row index
+      if (nr < N) dp[nr][c] += currentPath; // 도착 칸에 경우의 수 누적
 
-      const nc = c + move;
-      if (nc < N) dp[r][nc] += currentPath;
+      const nc = c + move; // 오른쪽으로 점프할 때 도착 col index
+      if (nc < N) dp[r][nc] += currentPath; // 도착 칸에 경우의 수 누적
     }
   }
 
-  return dp.at(-1).at(-1).toString();
+  return dp.at(-1).at(-1).toString(); // 마지막 칸의 경우의 수 BigInt를 String으로 변환
 };
 
 const fs = require('fs');
