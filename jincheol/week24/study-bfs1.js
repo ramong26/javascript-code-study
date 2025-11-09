d2120642287a0c5fea2248df3ba3da99a77ab203
jinoc <choiae369@gmail.com>
2025-11-10 01:04:04 +0900

    week24-아기 상어 2 주석 추가

--- jincheol/week24/study-bfs1.js
+++ jincheol/week24/study-bfs1.js
@@ -9,6 +9,7 @@
  * @param {number[][]} board 공간의 상태
  */
 const solution = (N, M, board) => {
+  // 이동하는 index
   const move = [
     [-1, -1],
     [-1, 0],
@@ -20,35 +21,36 @@ const solution = (N, M, board) => {
     [1, 1],
   ];
 
-  const safeDistance = Array.from({ length: N }, () => new Array(M).fill(0));
+  const safeDistance = Array.from({ length: N }, () => new Array(M).fill(0)); // 거리를 저장할 배열
 
-  const queue = [];
-
+  const queue = []; // bfs 큐
+  // 상어 위치를 시작 지점으로 설정
   for (let r = 0; r < N; r++) {
     for (let c = 0; c < M; c++) {
+      // 상어면
       if (board[r][c] === 1) {
-        queue.push([r, c]);
-        safeDistance[r][c] = 1;
+        queue.push([r, c]); // 큐에 추가
+        safeDistance[r][c] = 1; // 거리를 1로 시작
       }
     }
   }
 
-  let idx = 0;
-  let max = 0;
+  let idx = 0; // queue의 포인터
+  let max = 0; // 최대 거리
 
   while (idx < queue.length) {
-    const [r, c] = queue[idx++];
-    let curDistance = safeDistance[r][c];
-
+    const [r, c] = queue[idx++]; // 방문지 추출 + idx 늘리기
+    let curDistance = safeDistance[r][c]; // r c의 안전 거리
+    // 8방향 이동
     for (const [dr, dc] of move) {
       const nr = r + dr;
       const nc = c + dc;
-      if (nr < 0 || N <= nr || nc < 0 || M <= nc) continue;
-
+      if (nr < 0 || N <= nr || nc < 0 || M <= nc) continue; // 보드를 벗어나면 건너뛰기
+      // 방문하지 않은 칸일 경우
       if (safeDistance[nr][nc] === 0) {
-        safeDistance[nr][nc] = curDistance + 1;
-        queue.push([nr, nc]);
-        max = Math.max(max, safeDistance[nr][nc] - 1);
+        safeDistance[nr][nc] = curDistance + 1; // 안전 거리 + 1 저장
+        queue.push([nr, nc]); // 큐에 방문지 추가
+        max = Math.max(max, safeDistance[nr][nc] - 1); // 최댓값 갱신
       }
     }
   }
