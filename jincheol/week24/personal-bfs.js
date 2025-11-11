aae52ecbd8e20370d0982c66f882c816704a6275
jinoc <choiae369@gmail.com>
2025-11-11 16:13:22 +0900

    week24-스타트링크

--- jincheol/week24/personal-bfs.js
+++ jincheol/week24/personal-bfs.js
@@ -1,8 +1,40 @@
-// 문제 링크: [](https://www.acmicpc.net/problem/)
+// 문제 링크: [스타트링크](https://www.acmicpc.net/problem/5014)
 
 // 실행: node jincheol/week24/personal-bfs.js
 
-const solution = () => {};
+/**
+ *
+ * @param {number} F 건물의 층 수
+ * @param {number} S 현재 위치한 층
+ * @param {number} G 스타트링크가 위치한 층
+ * @param {number} U 위로 U층을 가는 버튼
+ * @param {number} D 아래로 D층을 가는 버튼
+ */
+const solution = (F, S, G, U, D) => {
+  if (S === G) return 0;
+
+  const visited = new Array(F + 1).fill(0);
+  const queue = [S];
+  visited[S] = 1;
+  let idx = 0;
+
+  while (idx < queue.length) {
+    const curFloor = queue[idx++];
+    const curCount = visited[curFloor];
+
+    const nextFloors = [curFloor + U, curFloor - D];
+    for (const next of nextFloors) {
+      if (next === G) return curCount;
+
+      if (next > 0 && next <= F && visited[next] === 0) {
+        visited[next] = curCount + 1;
+        queue.push(next);
+      }
+    }
+  }
+
+  return 'use the stairs';
+};
 
 const fs = require('fs');
 const path = require('path');
@@ -11,3 +43,6 @@ const filePath =
     ? '/dev/stdin'
     : path.join(__dirname, 'input3.txt');
 const input = fs.readFileSync(filePath).toString('utf8').trim().split('\n');
+
+const [F, S, G, U, D] = input[0].split(' ').map(Number);
+console.log(solution(F, S, G, U, D));
