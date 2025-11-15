99c1a2303f22fc244ef21e2031d943f0cd52800c
jinoc <choiae369@gmail.com>
2025-11-16 01:18:57 +0900

    week25-문자열 폭발

--- jincheol/week25/personal-dataStructure.js
+++ jincheol/week25/personal-dataStructure.js
@@ -2,7 +2,38 @@
 
 // 실행: node jincheol/week25/personal-dataStructure.js
 
-const solution = () => {};
+/**
+ *
+ * @param {string} strings
+ * @param {string} boom
+ */
+const solution = (strings, boom) => {
+  const stack = [];
+  const N = boom.length;
+
+  for (let str of strings) {
+    stack.push(str);
+    if (stack.length < N) continue;
+
+    const checkString = stack.slice(-N).join('');
+    let needBoom = checkString === boom;
+
+    while (needBoom) {
+      stack.splice(-N);
+      if (stack.length < N) {
+        needBoom = false;
+        break;
+      }
+
+      const nextCheck = stack.slice(-N).join('');
+      if (nextCheck !== boom) needBoom = false;
+    }
+  }
+
+  if (stack.length === 0) return 'FRULA';
+
+  return stack.join('');
+};
 
 const fs = require('fs');
 const path = require('path');
@@ -11,3 +42,7 @@ const filePath =
     ? '/dev/stdin'
     : path.join(__dirname, 'input3.txt');
 const input = fs.readFileSync(filePath).toString('utf8').trim().split('\n');
+
+const strings = input[0].trim();
+const boom = input[1];
+console.log(solution(strings, boom));
