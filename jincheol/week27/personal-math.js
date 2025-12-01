73fe8d8b53ec1acf2b8e8deb0369f28b303c6b82
jinoc <choiae369@gmail.com>
2025-12-01 13:36:40 +0900

    week27-피보나치 수 2

--- jincheol/week27/personal-math.js
+++ jincheol/week27/personal-math.js
@@ -1,8 +1,25 @@
-// 문제 링크: [](https://www.acmicpc.net/problem/)
+// 문제 링크: [피보나치 수 2](https://www.acmicpc.net/problem/2748)
 
 // 실행: node jincheol/week27/personal-math.js
 
-const solution = () => {};
+/**
+ *
+ * @param {number} N 구해야 하는 N번째 피보나치 수
+ */
+const solution = (N) => {
+  const fibo = new Array(N + 1).fill(0n); // 피보나치 수를 저장할 배열 BigInt 사용
+  fibo[1] = 1n; // 1번째 피보나치 수는 1
+
+  // 0번째 피보나치 수는 0, 1번째는 1이므로 2번째 수부터 계산
+  for (let i = 2; i <= N; i++) {
+    const before1 = fibo[i - 2]; // 전전 수
+    const before2 = fibo[i - 1]; // 전 수
+    const current = before1 + before2; // i번째 피보나치 수
+    fibo[i] = current; // 값 할당
+  }
+
+  return fibo[N].toString(); // N번째 피보나치 수 반환
+};
 
 const fs = require('fs');
 const path = require('path');
@@ -11,3 +28,6 @@ const filePath =
     ? '/dev/stdin'
     : path.join(__dirname, 'input3.txt');
 const input = fs.readFileSync(filePath).toString('utf8').trim().split('\n');
+
+const N = parseInt(input[0]);
+console.log(solution(N));
